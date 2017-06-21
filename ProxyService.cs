62dcf6118951d940000c835973b6a2bad3efd7c3
Rextor Divenc <n.navidy.2000@gmail.com>
2017-06-21 14:06:45 +0430

    Add files via upload

---
+++ ProxyService.cs
@@ -0,0 +1,10 @@
+﻿using MSC.Brute;
+
+namespace MSC
+{
+    public class ProxyService
+    {
+        public static Proxy proxy = new Proxy();
+        public static bool UseInAllRequest = false;
+    }
+}
