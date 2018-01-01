d5a727fc329ecb6c1cb8a9f9e54a94a3b0890012
Rextor Divenc <n.navidy.2000@gmail.com>
2018-01-01 15:46:31 +0330

    Update TelegramService.cs

--- TelegramService.cs
+++ TelegramService.cs
@@ -30,11 +30,13 @@ namespace MSC
             ac.Username = ID;
             ac.Password = Token;
 
+            text = text.Replace("&", "%26");
             config.Method = Method.POST;
             string pst = "https://api.telegram.org/bot<TOKEN>/sendMessage?chat_id=<ID>&text=<TEXT>";
             config.DataSet = "<ID>*<TOKEN>";
             pst = Rer.ReplaceAccount(ac, pst, config);
-            config.LoginURL = pst.Replace("<TEXT>", text);
+            pst = pst.Replace("<TEXT>", text);
+            config.LoginURL = pst;
             config.AllowAutoRedirect = true;
             RequestManage send = Rer.GETData(config);
         }
